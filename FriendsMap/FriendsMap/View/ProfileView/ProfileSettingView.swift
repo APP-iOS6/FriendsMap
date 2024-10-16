21b5f012ea24196c88a7e7697c2af97cd4a54553
SeungWoo <kangsw1025@naver.com>
2024-10-17 00:08:49 +0900

    [refactor] : profileSettingView 사이즈 크기 변수 변경

--- FriendsMap/FriendsMap/View/ProfileView/ProfileSettingView.swift
+++ FriendsMap/FriendsMap/View/ProfileView/ProfileSettingView.swift
@@ -14,104 +14,105 @@ struct ProfileSettingView: View {
     
     @EnvironmentObject var authStore: AuthenticationStore
     @EnvironmentObject var profileStore: ProfileStore
-
+    
+    let screenWidth = UIScreen.main.bounds.width
+    let screenHeight = UIScreen.main.bounds.height
+    
     
     var body: some View {
-        GeometryReader { proxy in
-            VStack(spacing: 20) {
-                HStack {
-                    Image("logo_white")
-                        .resizable()
-                        .scaledToFit()
+        VStack(spacing: 20) {
+            HStack {
+                Image("logo_white")
+                    .resizable()
+                    .scaledToFit()
+            }
+            .frame(width: screenWidth * 0.5)
+            .padding(.top, screenHeight * 0.05)
+            
+            Text("프로필 설정")
+                .foregroundStyle(.white)
+                .font(.title)
+                .bold()
+                .padding(.bottom, screenHeight * 0.1)
+            
+            
+            Button {
+                isPresented = true
+            } label : {
+                ZStack {
+                    if profileImage != UIImage() {
+                        Image(uiImage: profileImage)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: screenWidth * 0.4)
+                            .clipShape(.circle)
+                        
+                    } else {
+                        Image("defaultProfile")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: screenWidth * 0.4)
+                            .clipShape(.circle)
+                        Image(systemName: "plus")
+                            .font(.system(size: 40, weight: .bold))
+                            .foregroundStyle(.gray)
+                            .offset(x : screenWidth * 0.13, y : screenHeight * 0.055)
+                    }
+                    
+                    
                 }
-                .frame(width: proxy.size.width * 0.5)
-                .padding(.top, proxy.size.height * 0.05)
-                
-                Text("프로필 설정")
-                    .foregroundStyle(.white)
-                    .font(.title)
-                    .bold()
-                    .padding(.bottom, proxy.size.height * 0.1)
+                .frame(width: screenWidth * 0.7, height: screenHeight * 0.18)
+            }
+            
+            HStack {
+                createTextField(placeholder: "닉네임", varName: $nickname, isSecure: false)
+                    .frame(width: screenWidth * 0.55)
                 
                 
                 Button {
-                    isPresented = true
+                    // 닉네임 중복 확인 로직
                 } label : {
                     ZStack {
-                        if profileImage != UIImage() {
-                            Image(uiImage: profileImage)
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: proxy.size.width * 0.4)
-                                .clipShape(.circle)
-                            
-                        } else {
-                            Image("defaultProfile")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: proxy.size.width * 0.4)
-                                .clipShape(.circle)
-                            Image(systemName: "plus")
-                                .font(.system(size: 40, weight: .bold))
-                                .foregroundStyle(.gray)
-                                .offset(x : proxy.size.width * 0.13, y : proxy.size.height * 0.055)
-                        }
-                        
-                        
-                    }
-                    .frame(width: proxy.size.width * 0.7, height: proxy.size.height * 0.18)
-                }
-                
-                HStack {
-                    createTextField(placeholder: "닉네임", varName: $nickname, isSecure: false)
-                        .frame(width: proxy.size.width * 0.55)
-                    
-                    
-                    Button {
-                        // 닉네임 중복 확인 로직
-                    } label : {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 10)
-                                .frame(width: proxy.size.width * 0.21, height: proxy.size.height * 0.065)
-                                .foregroundStyle(Color(red: 147/255, green: 147/255, blue: 147/255))
-                            Text("중복확인")
-                                .foregroundStyle(.white)
-                                .font(.system(size: 20))
-                        }
-                        .padding(.vertical)
-                    }
-                }
-                .padding(.top, proxy.size.height * 0.02)
-                
-                Spacer()
-                
-                Button {
-                    // 유저 정보 관리하는 vm에서 로그인 했다고 업데이트하기
-                    Task {
-                        let result = await profileStore.updateProfile(nickname: nickname, image: "테스트수민", email: authStore.user!.email)
-                        if result {
-                            authStore.flow = .main
-                        }
-                    }
-                    //이미지 url 변환 필요함! 나중에 추가
-                } label: {
-                    ZStack {
                         RoundedRectangle(cornerRadius: 10)
-                            .frame(width: proxy.size.width * 0.5, height: proxy.size.height * 0.05)
+                            .frame(width: screenWidth * 0.21, height: screenHeight * 0.065)
                             .foregroundStyle(Color(red: 147/255, green: 147/255, blue: 147/255))
-                        Text("시작하기")
-                            .font(.system(size: 24))
+                        Text("중복확인")
                             .foregroundStyle(.white)
+                            .font(.system(size: 20))
                     }
-                    .padding(.bottom, proxy.size.height * 0.06)
+                    .padding(.vertical)
                 }
-                
             }
-            .frame(width:proxy.size.width, height: proxy.size.height)
-            .background(.loginViewBG)
-            .sheet(isPresented: $isPresented) {
-                ImagePicker(sourceType: .photoLibrary, selectedImage: self.$profileImage)
+            .padding(.top, screenHeight * 0.02)
+            
+            Spacer()
+            
+            Button {
+                // 유저 정보 관리하는 vm에서 로그인 했다고 업데이트하기
+                Task {
+                    let result = await profileStore.updateProfile(nickname: nickname, image: "테스트수민", email: authStore.user!.email)
+                    if result {
+                        authStore.flow = .main
+                    }
+                }
+                //이미지 url 변환 필요함! 나중에 추가
+            } label: {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: screenWidth * 0.5, height: screenHeight * 0.05)
+                        .foregroundStyle(Color(red: 147/255, green: 147/255, blue: 147/255))
+                    Text("시작하기")
+                        .font(.system(size: 24))
+                        .foregroundStyle(.white)
+                }
+                .padding(.bottom, screenHeight * 0.06)
             }
+            
+        }
+        .frame(width:screenWidth, height: screenHeight)
+        .background(.loginViewBG)
+        .sheet(isPresented: $isPresented) {
+            ImagePicker(sourceType: .photoLibrary, selectedImage: self.$profileImage)
         }
     }
 }
