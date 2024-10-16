f1f51ef851b6e748aa154ff9703350e049fc51a8
ParkJunYoung <j77777y@naver.com>
2024-10-16 15:43:57 +0900

    [feat]: 로직 수정

--- FriendsMap/FriendsMap/ViewModel/MainViewModel.swift
+++ FriendsMap/FriendsMap/ViewModel/MainViewModel.swift
@@ -20,13 +20,13 @@ class MainViewModel: ObservableObject {
             let db = Firestore.firestore()
             let userCollection = try await db.collection("User").document("j77777y@naver.com").collection("Contents").getDocuments()
             
-            var savedPosts: [Content] = [] // 임시 저장 공간
+            // var savedPosts: [Content] = [] // 임시 저장 공간
             
             for document in userCollection.documents {
                 let id: String = document.documentID
                 
                 let docData = document.data()
-                let image = docData["image"] as? String ?? ""
+                
                 let text = docData["text"] as? String ?? ""
                 let likeCount = docData["likeCount"] as? Int ?? 0
                 
@@ -36,15 +36,30 @@ class MainViewModel: ObservableObject {
                 let latitude = docData["latitude"] as? Double ?? 0.0
                 let longitude = docData["longitude"] as? Double ?? 0.0
                 
-                let post = Content(id: id, image: image, text: text, likeCount: likeCount, contentDate: contentDate, latitude: latitude, longitude: longitude)
-                savedPosts.append(post)
+                let image = docData["image"] as? String ?? ""
+                let storageRef = Storage.storage().reference(withPath: "\(image)")
+                
+                let downloadURL = try await getDownloadURL(for: storageRef)
+                print(downloadURL)
+                
+                self.userPost.append(Content(id: id, image: downloadURL, text: text, likeCount: likeCount, contentDate: contentDate, latitude: latitude, longitude: longitude))
             }
             
-            userPost = savedPosts
             print("유저 데이터 : \(userPost)")
             
         } catch {
             print("유저 데이터 가져오기 실패: \(error)")
+        }
+    }
+    
+    func getDownloadURL(for imageReference: StorageReference) async throws -> String {
+        do {
+            // 다운로드 URL 가져오기 (async/await)
+            let url = try await imageReference.downloadURL()
+            return url.absoluteString
+        } catch let error {
+            print("Error getting download URL: \(error)")
+            throw error
         }
     }
 }
