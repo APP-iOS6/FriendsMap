2e1a95df08e7664662da0ffa8009a8fc184f0925
SeungWoo <kangsw1025@naver.com>
2024-10-19 00:44:52 +0900

    [feat] : loginView textfield 선택시 로고 올라가는 거 방지 및 스크롤 가능하게 변경

--- FriendsMap/FriendsMap/Modifier/LoginView/LoginTextFieldModifier.swift
+++ FriendsMap/FriendsMap/Modifier/LoginView/LoginTextFieldModifier.swift
@@ -21,3 +21,9 @@ struct LoginTextFieldModifier: TextFieldStyle {
             }
     }
 }
+
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
