b99f5ea103ba3faec7c162cda3969fdfd06a4000
Soomin Kim <140694831+sumchive@users.noreply.github.com>
2024-10-20 20:22:52 +0900

    [feat] : 프로필 관리에서 유저 프사 변경 기능 추가

--- FriendsMap/FriendsMap/View/ProfileView/ProfileManagementView.swift
+++ FriendsMap/FriendsMap/View/ProfileView/ProfileManagementView.swift
@@ -9,35 +9,91 @@ import SwiftUI
 
 struct ProfileManagementView: View {
     @EnvironmentObject private var userViewModel: UserViewModel
+    @EnvironmentObject private var authStore: AuthenticationStore
+    
+    @State private var profileImage: UIImage = UIImage()
+    @State private var isPresented: Bool = false
+    
+    let screenWidth = UIScreen.main.bounds.width
+    let screenHeight = UIScreen.main.bounds.height
+    
     var body: some View {
         NavigationStack {
-            ZStack {
-                VStack {
-                    Circle()
-                        .fill(.orange.gradient)
-                        .frame(width: 200,height: 200)
-                        .overlay {
+            VStack {
+                Spacer()
+                Button {
+                    isPresented = true
+                } label : {
+                    ZStack {
+                        if let photos = userViewModel.profile?.image {
+                            AsyncImage(url: URL(string: photos)!) { image in
+                                image.image?
+                                    .resizable()
+                                    .frame(width: screenWidth * 0.4, height: screenWidth * 0.4)
+                                    .clipShape(Circle())
+                                    .aspectRatio(contentMode: .fit)
+                            }
                             Image(systemName: "pencil.circle.fill")
-                                .symbolRenderingMode(.multicolor)
-                                .font(.system(size: 30))
-                                .foregroundColor(.accentColor)
-                        }
-                    Text("설정")
-                    
-                    List (0..<8) { index in
-                        NavigationLink {
-                            Text("\(index)")
-                        }label: {
-                            Text("설정 내용")
+                                .font(.system(size: 40, weight: .bold))
+                                .foregroundStyle(.gray)
+                                .offset(x : screenWidth * 0.13, y : screenHeight * 0.055)
                         }
                     }
-                    .listStyle(.inset)
                 }
+                if let nickname = userViewModel.profile?.nickname {
+                    Text(nickname)
+                        .font(.title3)
+                        .foregroundStyle(.white)
+                        .shadow(color: .black, radius: 2, x: 1, y: 1)
+                }
+                //                Text("설정")
+                //
+                //                List (0..<8) { index in
+                //                    NavigationLink {
+                //                        Text("\(index)")
+                //                    }label: {
+                //                        Text("설정 내용")
+                //                    }
+                //                }
+                //                .listStyle(.inset)
+                
+                Spacer()
+                
+                Button {
+                    Task {
+                        let imageData = profileImage.jpegData(compressionQuality: 0.8)
+                        let result = await authStore.updateProfile(nickname: userViewModel.profile?.nickname ?? "", image: imageData, email: authStore.user!.email)
+                        if result {
+                            print("업데이트 성공")
+                        } else {
+                            print("업데이트 안됨")
+                        }
+                    }
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(width: screenWidth * 0.5, height: screenHeight * 0.06)
+                            .foregroundStyle(Color(red: 147/255, green: 147/255, blue: 147/255))
+                        Text("변경하기")
+                            .font(.system(size: 24))
+                            .foregroundStyle(.white)
+                    }
+                    .padding(.bottom, screenHeight * 0.15)
+                }
+                
+                .padding(.horizontal, 27)
+            }
+            .frame(width:screenWidth, height: screenHeight)
+            .background(.loginViewBG)
+            .sheet(isPresented: $isPresented) {
+                ImagePicker(sourceType: .photoLibrary, selectedImage: self.$profileImage)
+                    .onChange(of: profileImage) { _, _ in
+                        
+                    }
             }
         }
     }
 }
-
 #Preview {
     ProfileManagementView()
 }
